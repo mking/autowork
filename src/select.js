09a6b27b5f5b69b77cc98f07f535941c17146efc
Matthew King <mking@users.noreply.github.com>
2020-06-06 04:05:03 -0700

    fix select

--- src/select.js
+++ src/select.js
@@ -3,10 +3,11 @@ import { submitDialog } from "./common";
 export const killSelect = (element) => {
   if ($(element).find("select").length) {
     const selectText = $(element).find('p:contains("1. Select")').text();
-    const selectMatch = /^1\. Select (\w*)$/.exec(selectText);
+    const selectMatch = /^1\. Select (.+)$/.exec(selectText);
     if (selectMatch) {
       const optionText = selectMatch[1];
       $(element).find("select").val(optionText);
+      $(element).find("select").selectmenu("refresh");
 
       submitDialog(element);
     }
