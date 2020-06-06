8be0fdd59e0658a1151902d83a5f309f7861a264
Matthew King <mking@users.noreply.github.com>
2020-06-06 04:18:39 -0700

    fix slider bug

--- src/slider.js
+++ src/slider.js
@@ -3,7 +3,7 @@ import { submitDialog } from "./common";
 export const killSlider = (element) => {
   if ($(element).find('p:contains("1. Set slider to")').length) {
     const sliderText = $(element).find('p:contains("1. Set slider to")').text();
-    const sliderMatch = /^1\. Set slider to (\d+)$/.exec(sliderText);
+    const sliderMatch = /^1\. Set slider to (\d+)/.exec(sliderText);
     if (sliderMatch) {
       const valueText = sliderMatch[1];
       $(element).find(".ui-slider").slider("option", "value", valueText);
